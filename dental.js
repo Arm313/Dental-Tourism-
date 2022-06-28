f8e6da7281f67a8d7b2e7983f23193f61604664c
Arm313 <armbabujyan@gmail.com>
2022-06-28 18:51:42 +0400

    bugs corrected

--- dental.js
+++ dental.js
@@ -1,19 +1,35 @@
 const menu = document.querySelector(".menu");
 const navbar = document.querySelector(".navbar-ul");
-const close = document.querySelector('.close');
-
-
-
+const close = document.querySelector(".close");
 
 menu.addEventListener("click", () => {
   navbar.style.display = "block";
-  navbar.style.right = "1px";
+  navbar.style.right = "0px";
   menu.style.display = "none";
-  
 });
 close.addEventListener("click", () => {
   navbar.style.display = "none";
   navbar.style.right = "-300px";
   menu.style.display = "block";
-  
 });
+
+// window.addEventListener(
+//   "resize",
+//   function () {
+//     if ((navbar.style.display = "block")) {
+//       navbar.style.display = "none";
+      
+      
+      
+//     }
+//     navbar.style.flexDirection = "row";
+
+
+//     // navbar.style.display = "block";
+//     // menu.style.display = "none";
+//     // navbar.style.flexDiraction = "row";
+//     // console.log("addEventListener - resize");
+//     // alert("addEventListener - resize");
+//   }
+//   // true
+// );
